792e878a45c0623ee2f655a3bc5b6de9bbe64e6b
Shaal0wFR <65244389+Shaal0wFR@users.noreply.github.com>
2021-08-29 00:34:44 +0200

    Add files via upload

---
+++ unbonwhiskybots.sh
@@ -0,0 +1,32 @@
+Actual_IP=$(curl -s https://api.ipify.org)
+
+email="" # Your email of connexion to your cloudflare account
+global_key="" # Can be found on https://dash.cloudflare.com/profile/api-tokens
+
+Zone_ID="" # It is shown at the bottom of the preview tab
+Domain_Name="" # Put the name of the domain you want to update the IP. It is normally on the right of the Type
+proxy="" # If your website is proxied, put "true", else : "false"
+type="A" # Your DNS Type. Default is A
+
+Get_IP=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${Zone_ID}/dns_records?type=${type}&name=${Domain_Name}&proxied=${proxy}" \
+     -H "X-Auth-Email: ${email}" \
+     -H "X-Auth-Key: ${global_key}" \
+     -H "Content-Type: application/json" | \
+     jq -r .result[0].content)
+
+if [ $Actual_IP = $Get_IP ]
+then
+	set -e
+else
+	Get_ID=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/${Zone_ID}/dns_records?type=${type}&name=${Domain_Name}&proxied=${proxy}" \
+     		-H "X-Auth-Email: ${email}" \
+     		-H "X-Auth-Key: ${global_key}" \
+     		-H "Content-Type: application/json" | \
+     		jq -r .result[0].id)
+
+	curl -X PUT "https://api.cloudflare.com/client/v4/zones/${Zone_ID}/dns_records/${Get_ID}" \
+     		-H "X-Auth-Email: ${email}" \
+     		-H "X-Auth-Key: ${global_key}" \
+     		-H "Content-Type: application/json" \
+     		--data "{\"type\":\"${type}\",\"name\":\"${Domain_Name}\",\"content\":\"$Actual_IP\",\"proxied\":${proxy}}"
+fi
